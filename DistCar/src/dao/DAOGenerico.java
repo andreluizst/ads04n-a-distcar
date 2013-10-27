bc0c1807da18051fc56df7c772f0b42221a5015a
andre.luiz.silvat@gmail.com <andre.luiz.silvat@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-10-27 05:26:58 +0000

    DAOGenerico e IDAOGenerico atualizados para versão dada por Francisco. Adicionado o pacote util com duas classes.

--- DistCar/src/dao/DAOGenerico.java
+++ DistCar/src/dao/DAOGenerico.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.lang.reflect.ParameterizedType;
+
 import java.util.Collection;
 import java.util.List;
 
@@ -9,9 +10,15 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-//import javax.persistence.criteria.CriteriaBuilder;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
 
+import util.Parametros;
 
 /**
  * PSC
@@ -22,13 +29,11 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 
 	protected EntityManager entityManager;
 	protected Class<Entidade> classePersistente;
-//	protected CriteriaBuilder criteriaBuilder;
 
 	@SuppressWarnings("unchecked")
 	public DAOGenerico(){
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("DistCar");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(Parametros.UNIT_PERSISTENCE_NAME);
 		entityManager = emf.createEntityManager();
-//		criteriaBuilder = entityManager.getCriteriaBuilder();
 		
 		ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();  
 	    classePersistente = (Class<Entidade>) parameterizedType.getActualTypeArguments()[0];  
@@ -41,11 +46,11 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 	 *            a ser realizado o merge
 	 * @return objeto que foi executado o merge
 	 */
-	public void alterar(Entidade objeto) {
+	public final void alterar(Entidade objeto) {
 		EntityTransaction tx = getEntityManager().getTransaction();
 		try {
 			tx.begin();
-			
+			 
 			objeto = getEntityManager().merge(objeto);
 			
 			tx.commit();
@@ -63,7 +68,7 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 	 * 
 	 * @param objeto a ser salvo
 	 */
-	public void inserir(Entidade objeto) {
+	public final void inserir(Entidade objeto) {
 		EntityTransaction tx = getEntityManager().getTransaction();		
 		try {
 			tx.begin();
@@ -110,7 +115,7 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 	 * @param objeto
 	 *            a ser removido
 	 */
-	public void remover(Entidade objeto) {
+	public final void remover(Entidade objeto) {
 		EntityTransaction tx = getEntityManager().getTransaction();
 		try {
 			tx.begin();
@@ -160,6 +165,113 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 	}
 
 	/**
+	 * Busca o objeto de acordo com o objeto preenchido com os valores passado
+	 * como exemplo.
+	 * 
+	 * @param objeto
+	 *            utilizado para realizar a busca
+	 * @param ordenacoes
+	 *            lista de critérios de ordenação
+	 * @return Lista de objetos retornada
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public final List<Entidade> pesquisar(Entidade objeto) {
+		Example example = criaExemplo(objeto);
+		Criteria criteria = criaCriteria().add(example);
+		criteria.add(example);
+		return (List<Entidade>) criteria.list();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final List<Entidade> pesquisar(Entidade objeto, Order... ordenacoes) {
+		Example example = criaExemplo(objeto);
+		Criteria criteria = criaCriteria().add(example);
+		for(Order ord : ordenacoes){
+			criteria.addOrder(ord);
+		}
+		return (List<Entidade>) criteria.list();
+	}
+
+	/**
+	 * Busca o objeto de acordo com o objeto preenchido com os valores passado
+	 * como exemplo.
+	 * 
+	 * @param objeto
+	 * @param indiceInicial
+	 * @param indiceFinal
+	 * @param ordenacoes
+	 *            lista de critérios de ordenação.
+	 * @return Lista de orden
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public final List<Entidade> pesquisar(Entidade objeto, Integer indiceInicial,
+			Integer quantidade) {
+		Example example = criaExemplo(objeto);
+		Criteria criteria = criaCriteria().add(example);
+		criteria.setFirstResult(indiceInicial);
+		criteria.setMaxResults(quantidade);
+
+		return (List<Entidade>) criteria.list();
+	}
+
+	@Override 
+	@SuppressWarnings("unchecked")
+	public final List<Entidade> pesquisar(Entidade objeto, Integer indiceInicial,
+			Integer quantidade, Order... ordenacoes) {
+		Example example = criaExemplo(objeto);
+		Criteria criteria = criaCriteria().add(example);
+		criteria.setFirstResult(indiceInicial);
+		criteria.setMaxResults(quantidade);
+		for(Order ord : ordenacoes){
+			criteria.addOrder(ord);
+		}
+		return (List<Entidade>) criteria.list();
+	}
+	
+	/**
+	 * Método utilizado para criar o objeto Example. Este objeto é utilizado
+	 * para realizar a busca por exemplo.
+	 * 
+	 * @param objeto
+	 *            sobre o qual o Example será criado
+	 * @return em objeto do tipo Example
+	 */
+	protected final Example criaExemplo(Entidade objeto) {
+
+		Example example = Example.create(objeto);
+		example.enableLike(MatchMode.ANYWHERE);
+		example.excludeZeroes();
+		example.ignoreCase();
+
+		return example;
+	}
+
+	/**
+	 * Retorna o objeto da clases Criteria.
+	 * 
+	 * @return um objeto do tipo Criteria do Hibernate
+	 */
+	protected final Criteria criaCriteria() {
+		Session session = (Session) getEntityManager().getDelegate();
+		return session.createCriteria(classePersistente);
+	}
+
+	/**
+	 * Retorna a quantidade total de objetos para aquela entidade específica.
+	 * 
+	 * @return quantidade total de objetos
+	 */
+	public final int buscaQuantidadeTotal() {
+		Criteria criteria = criaCriteria();
+		criteria.setProjection(Projections.rowCount());
+		return ((Long) criteria.uniqueResult()).intValue();
+	}
+	
+	
+	/**
 	 * Atualiza o objeto que se encontra em memória.
 	 * 
 	 * @param object
@@ -183,5 +295,5 @@ public abstract class DAOGenerico<Entidade> implements IDAOGenerico<Entidade>{
 		return entityManager;
 	}
 
-			
+		
 }
--- DistCar/src/dao/IDAOGenerico.java
+++ DistCar/src/dao/IDAOGenerico.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.util.List;
 
+import org.hibernate.criterion.Order;
+
 public interface IDAOGenerico<Entidade> {
 
 	public void inserir(Entidade entidade);
@@ -13,5 +15,22 @@ public interface IDAOGenerico<Entidade> {
 	public Entidade consultarPorId(Integer id);
 	
 	public List<Entidade> consultarTodos();
+
+	/**
+	 * Pesquisa por exemplo
+	 * @param exemplo - Preencha os atributos utilizados na pesquisa
+	 * @param indiceInicial
+	 * @param quantidade
+	 * @param ordenacoes
+	 * @return
+	 */
+	public List<Entidade> pesquisar(Entidade exemplo, Integer indiceInicial, Integer quantidade,
+			Order... ordenacoes);
+
+	public List<Entidade> pesquisar(Entidade exemplo, Integer indiceInicial, Integer quantidade);
+
+	public List<Entidade> pesquisar(Entidade exemplo, Order... ordenacoes);
+
+	public List<Entidade> pesquisar(Entidade exemplo);
 
 }
