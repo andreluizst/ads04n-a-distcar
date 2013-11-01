351215921b328ef9a87877bb400c6b2cee5db0b4
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-01 22:15:57 +0000

    Atualização IDAOCarro

--- DistCar/src/dao/IDAOCarro.java
+++ DistCar/src/dao/IDAOCarro.java
@@ -5,14 +5,14 @@ import java.util.List;
 import classesBasicas.AcessorioCarro;
 import classesBasicas.Carro;
 import classesBasicas.ModeloCarro;
-import classesBasicas.VersaoModeloCarro;
+import classesBasicas.VersaoCarro;
 
 
 public interface IDAOCarro extends IDAOGenerico<Carro>{
 
 	public List<Carro> pesquisarCarroPorChassi(String chassi);
 	public List<Carro> pesquisarCarroPorModelo(ModeloCarro modeloCarro);
-	public List<Carro> pesquisarCarroPorVersaoCarro(VersaoModeloCarro versaoModeloCarro);
+	public List<Carro> pesquisarCarroPorVersaoCarro(VersaoCarro versaoCarro);
 	public List<Carro> pesquisarCarroPorItemSerie(String descricao);
 	public List<Carro> pesquisarCarroPorAcessorioCarro(AcessorioCarro acessorioCarro);
 	public List<Carro> emitirRelatorio();
