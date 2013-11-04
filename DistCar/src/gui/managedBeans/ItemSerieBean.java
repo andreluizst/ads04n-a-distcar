a9ba52301092c9a6ed202ec3dcdc236a3cc73386
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-04 02:25:15 +0000

    Atualização ItemSerieBean

--- DistCar/src/gui/managedBeans/ItemSerieBean.java
+++ DistCar/src/gui/managedBeans/ItemSerieBean.java
@@ -1,48 +1,62 @@
 package gui.managedBeans;
 
+import java.awt.event.ActionEvent;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.event.ActionEvent;
-
 import classesBasicas.ItemSerieCarro;
 import classesBasicas.ModeloCarro;
 import classesBasicas.Situacao;
 import fachada.Fachada;
+import gui.MsgPrimeFaces;
+
+
 
 @ManagedBean
 @SessionScoped
 public class ItemSerieBean {
 
-	private ItemSerieCarro itemSerieCarro = new ItemSerieCarro();
+	private ItemSerieCarro itemSerieCarro;
 	private List<ModeloCarro> modeloCarros;
 	private int codigoSelecionado;
 	public String mensagem;
 	private Date data;
 	private List<ItemSerieCarro> listaItens;
-
-
-
-	public ItemSerieBean(ItemSerieCarro itemSerieCarro,
-			List<ModeloCarro> modeloCarros, int codigoSelecionado,
-			String mensagem, Date data) {
-		super();
-		this.itemSerieCarro = itemSerieCarro;
-		this.modeloCarros = modeloCarros;
-		this.codigoSelecionado = codigoSelecionado;
-		this.mensagem = mensagem;
-		this.data = data;
+	private ItemSerieCarro itemSelecionado;
+	private Fachada f;
+	private ItemSerieCarro situacaoSelecionada;
+	private Situacao[] situacoes = Situacao.values();
+	
+	
+	public ItemSerieCarro getSituacaoSelecionada() {
+		return situacaoSelecionada;
 	}
 
-	public List<ItemSerieCarro> getListaItens() {
-		return listaItens;
+	public void setSituacaoSelecionada(ItemSerieCarro situacaoSelecionada) {
+		this.situacaoSelecionada = situacaoSelecionada;
 	}
 
-	public void setListaItens(List<ItemSerieCarro> listaItens) {
-		this.listaItens = listaItens;
+	public Situacao[] getSituacoes() {
+		return situacoes;
+	}
+
+	public void setSituacoes(Situacao[] situacoes) {
+		this.situacoes = situacoes;
+	}
+
+	public ItemSerieCarro getItemSelecionado() {
+		return itemSelecionado;
+	}
+
+	public void setItemSelecionado(ItemSerieCarro itemSelecionado) {
+		this.itemSelecionado = itemSelecionado;
+	}
+
+
+	public void novo(ActionEvent actionEvent) {
+		itemSerieCarro = new ItemSerieCarro();
 	}
 
 	public ItemSerieCarro getItemSerieCarro() {
@@ -85,25 +99,40 @@ public class ItemSerieBean {
 		this.data = data;
 	}
 
-	public void salvar() throws Exception {
-
-		Fachada f = new Fachada();
-		itemSerieCarro.setModeloCarro(f
-				.pesquisarModeloCarro(codigoSelecionado));
-		//Adicionar os setSituação e setDataAtualizacao na regra de negocio;
-		itemSerieCarro.setSituacao(Situacao.ATIVO);
-		itemSerieCarro.setDataUltimaAtualizacao(Calendar.getInstance());
-		f.salvarItemSerie(itemSerieCarro);
-		mensagem = "Item de série salvo som Sucesso";
-		itemSerieCarro = new ItemSerieCarro();
-		listaItens = f.listarItem();
-	
-		
+	public List<ItemSerieCarro> getListaItens() {
+		return listaItens;
 	}
 
+	public void setListaItens(List<ItemSerieCarro> listaItens) {
+		this.listaItens = listaItens;
+	}
+
+	private List<ItemSerieCarro> listarItens() {  
+      listaItens = Fachada.obterInstancia().listarItem();
+      return listaItens;
+      } 
+	
+	public String salvar() throws Exception {
+		
+		itemSerieCarro.setModeloCarro(f.pesquisarModeloCarro(codigoSelecionado));
+		//Adicionar os setSituação e setDataAtualizacao na regra de negocio;
+		//itemSerieCarro.setSituacao(situacaoSelecionada);
+		itemSerieCarro.setDataUltimaAtualizacao(Calendar.getInstance());
+		f.salvarItemSerie(itemSerieCarro);
+		MsgPrimeFaces.exibirMensagemInfomativa("Item Séria salvo com sucesso!");
+		itemSerieCarro = new ItemSerieCarro();
+		consulta();
+		return "item";
+	
+	}
+		
+	public void listar(){
+		listaItens = Fachada.obterInstancia().listarItem();
+	}
+		
 	public List<ModeloCarro> listarModelo() {
 		try {
-			modeloCarros = Fachada.obterInstancia().listarModelo();
+			modeloCarros = f.listarModelo();
 			return modeloCarros;
 		} catch (Exception ex) {
 			mensagem = ex.getMessage();
@@ -112,24 +141,79 @@ public class ItemSerieBean {
 	}
 
 	public ItemSerieBean() {
-
+		f = new Fachada();
 		itemSerieCarro = new ItemSerieCarro();
 		listarModelo();
 		data = Calendar.getInstance().getTime();
 		listarItens();
 	}
 
-	public void novo(ActionEvent actionEvent) {
-		itemSerieCarro = new ItemSerieCarro();
-	}
-
-	private List<ItemSerieCarro> listarItens() {  
-      listaItens = Fachada.obterInstancia().listarItem();
-      return listaItens;
-         } 
-	
-	public void salvarAjax(ActionEvent actionEvent) throws Exception {
-		salvar();
+	public void excluir(){
+		if(itemSelecionado==null){
+			MsgPrimeFaces.exibirMensagemInfomativa("Selecione um item para exclusão!");
+		}
+		else{
+		itemSerieCarro = f.pesquisarItem(itemSelecionado.getCodigo());
+		f.removerItem(itemSerieCarro);
+		consulta();
+		}
 	}
 	
+	public void consulta(){
+		listaItens = f.listarItem();
+	}
+	public void Alterar(ItemSerieCarro itemSerieCarro) throws Exception{
+		
+		f.alterarItem(itemSerieCarro);
+		MsgPrimeFaces.exibirMensagemInfomativa("Item Série alterado com sucesso!");
+		this.itemSerieCarro = new ItemSerieCarro();
+		consulta();
+	}
+	
+	  /* public void onEdit(RowEditEvent event) throws Exception {  
+		   
+		   ItemSerieCarro i = (ItemSerieCarro) event.getObject();
+		   itemSerieCarro = Fachada.obterInstancia().pesquisarItem(i.getCodigo());
+		   Fachada.obterInstancia().salvarItemSerie(itemSerieCarro);
+      
+	    }  
+	      
+	    public void onCancel(RowEditEvent event) {  
+	        FacesMessage msg = new FacesMessage("Car Cancelled", "Cancelado");    
+	  
+	        FacesContext.getCurrentInstance().addMessage(null, msg);  
+	    } */
+	    public String novo(){
+	    	itemSerieCarro = new ItemSerieCarro();
+			return "novoItem";
+		}           
+	    
+	    public String alterar(){
+	    	if (itemSelecionado==null){
+	    		MsgPrimeFaces.exibirMensagemInfomativa("Selecione um item para alterar!");
+	    		return "item";
+	    	}
+	    	else{
+	    		System.out.println(itemSelecionado.toString());
+	    	itemSerieCarro = itemSelecionado;
+	    	
+	    	return "novoItem";
+	    	}
+	    }
+	    
+	    /*public String cancelar(){
+	    	itemSerieCarro = new ItemSerieCarro();
+	    	consulta();
+	    	return "item";
+	    }   */
+	    
+	    public void consultar(){
+	    
+	    	itemSerieCarro.setModeloCarro(f.pesquisarModeloCarro(codigoSelecionado));
+	    //	itemSerieCarro.setSituacao(situacaoSelecionada);
+	    	System.out.println(itemSerieCarro.toString());
+	    	listaItens = f.pesquisarItens(itemSerieCarro);
+			 itemSerieCarro = new ItemSerieCarro();   	
+	    }
+	    
 }
