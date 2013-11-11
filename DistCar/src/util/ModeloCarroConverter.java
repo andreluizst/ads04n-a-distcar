f7d4baa6ec3d7df61e9d5caf2fd270848627f2dc
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-11 03:11:47 +0000

    Atualização dos converters

---
+++ DistCar/src/util/ModeloCarroConverter.java
@@ -0,0 +1,39 @@
+package util;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+
+
+
+
+import classesBasicas.ModeloCarro;
+
+import fachada.Fachada;
+
+@FacesConverter(value="modeloCarroConverter",forClass=ModeloCarro.class)
+public class ModeloCarroConverter implements Converter {
+	
+	 public Object getAsObject(FacesContext context, UIComponent component, String value)  {     
+            if (value == null)   
+                return null;  
+            Integer id = new Integer(value);              
+            return Fachada.obterInstancia().pesquisarModelosCarroCodigo(id);  
+    }  
+  
+    public String getAsString(FacesContext context, UIComponent componente, Object value) {  
+        
+        if (value == null) 
+            return null;  
+        if (value instanceof ModeloCarro) {  
+            ModeloCarro aux = (ModeloCarro) value;  
+            return aux.getCodigo().toString();  
+        }  
+        else if(value instanceof String && ((String)value).equalsIgnoreCase("-1")){  
+            return "-1";  
+        }
+		return null; 
+    } 	
+}
