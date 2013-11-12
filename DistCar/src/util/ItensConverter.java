e556e5e29af343a9ea6f5d41b934205047e84fcf
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-12 03:32:03 +0000

    Criação do converter ItensConverter

---
+++ DistCar/src/util/ItensConverter.java
@@ -0,0 +1,33 @@
+package util;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import classesBasicas.ItemSerieCarro;
+import fachada.Fachada;
+
+@FacesConverter(value="ItensConverter",forClass=ItemSerieCarro.class)
+public class ItensConverter implements Converter {
+	
+	 public Object getAsObject(FacesContext context, UIComponent component, String value)  {     
+            if (value == null)   
+                return null;  
+            Integer id = new Integer(value);              
+            return Fachada.obterInstancia().pesquisarItemCodigo(id);  
+    }  
+  
+    public String getAsString(FacesContext context, UIComponent componente, Object value) {  
+        
+        if (value == null) 
+            return null;  
+        if (value instanceof ItemSerieCarro) {  
+            ItemSerieCarro aux = (ItemSerieCarro) value;  
+            return aux.getCodigo().toString();  
+        }  
+        else if(value instanceof String && ((String)value).equalsIgnoreCase("-1")){  
+            return "-1";  
+        }
+		return null; 
+    } 	
+}
