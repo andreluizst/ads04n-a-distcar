7e324910f9f0c2ecd46a880f918a9bbed832ed15
andre.luiz.silvat@gmail.com <andre.luiz.silvat@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-18 02:57:31 +0000

    validator para cpf e cnpj criados. CRUD Fabricante em andamento(50% funcional). Atributo showDetail das TAGs <p:messages> setadas para FALSE para evitar a repetição de mensagens no cabeçalho.

--- DistCar/src/gui/MsgPrimeFaces.java
+++ DistCar/src/gui/MsgPrimeFaces.java
@@ -6,41 +6,41 @@ import javax.faces.context.FacesContext;
 public class MsgPrimeFaces {
 	public static void exibirMensagemDeErro(String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"ERRO", mensagem));
+    			"ERRO", new FacesMessage(FacesMessage.SEVERITY_ERROR,"ERRO", mensagem));
     }
     
     public static void exibirMensagemDeErro(String titulo, String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_ERROR,titulo, mensagem));
+    			"ERRO", new FacesMessage(FacesMessage.SEVERITY_ERROR,titulo, mensagem));
     }
     
     public static void exibirMensagemInfomativa(String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação", mensagem));
+    			"Informação", new FacesMessage(FacesMessage.SEVERITY_INFO,"Informação", mensagem));
     }
     
     public static void exibirMensagemInfomativa(String titulo, String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, mensagem));
+    			"Informação", new FacesMessage(FacesMessage.SEVERITY_INFO, titulo, mensagem));
     }
     
     public static void exibirMensagemDeAviso(String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Informação", mensagem));
+    			"Aviso", new FacesMessage(FacesMessage.SEVERITY_WARN,"Informação", mensagem));
     }
     
     public static void exibirMensagemDeAviso(String titulo, String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_WARN, titulo, mensagem));
+    			"Aviso", new FacesMessage(FacesMessage.SEVERITY_WARN, titulo, mensagem));
     }
     
     public static void exibirMensagemFatal(String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"Informação", mensagem));
+    			"FALHA", new FacesMessage(FacesMessage.SEVERITY_FATAL,"FALHA", mensagem));
     }
     
     public static void exibirMensagemFatal(String titulo, String mensagem){
     	FacesContext.getCurrentInstance().addMessage(
-    			null, new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, mensagem));
+    			"FALHA", new FacesMessage(FacesMessage.SEVERITY_FATAL, titulo, mensagem));
     }
 }
