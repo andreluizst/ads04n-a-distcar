7e5e157498bd9dc3223374b289d7a0cbf24bf5b2
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-24 21:56:54 +0000

    Atualização Dao

--- DistCar/src/dao/DAOGenerico.java
+++ DistCar/src/dao/DAOGenerico.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 import org.hibernate.Criteria;
