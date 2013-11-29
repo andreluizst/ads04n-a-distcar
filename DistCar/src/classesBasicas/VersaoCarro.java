c99431dfb29a840372b09bc03b54ab780a6c2c01
fellipecarllos@gmail.com <fellipecarllos@gmail.com@2faee1ab-45a1-09f7-9623-9664e23e114c>
2013-11-29 22:54:54 +0000

    Altualização versaoCarro

--- DistCar/src/classesBasicas/VersaoCarro.java
+++ DistCar/src/classesBasicas/VersaoCarro.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 import javax.persistence.*;
 
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
-
 
 @Entity
 public class VersaoCarro extends ObjetoGeral {
@@ -26,10 +22,12 @@ public class VersaoCarro extends ObjetoGeral {
 	private String descricao;
 	@ManyToOne(fetch = FetchType.EAGER)
 	private ModeloCarro modeloCarro;
-	@ManyToMany(fetch = FetchType.LAZY)
-	@Fetch(FetchMode.SUBSELECT)
-	private List<AcessorioCarro> acessorios;
 	@ManyToMany
+	private List<AcessorioCarro> acessorios;
+	@ManyToMany(cascade={ CascadeType.ALL, CascadeType.MERGE }, fetch=FetchType.EAGER)
+	@JoinTable(name="versaocarro_itemseriecarro",     
+    joinColumns = @JoinColumn(name="VersaoCarro_codigo", referencedColumnName="codigo"),     
+    inverseJoinColumns = @JoinColumn(name="itens_codigo",referencedColumnName="codigo"))
 	private List<ItemSerieCarro> itens;
 	public double getValor() {
 		return valor;
@@ -81,56 +79,7 @@ public class VersaoCarro extends ObjetoGeral {
 				+ " " + getDescricao() + "[" + acessorios + " + " + itens + "]";
 		return str;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result
-				+ ((acessorios == null) ? 0 : acessorios.hashCode());
-		result = prime * result
-				+ ((descricao == null) ? 0 : descricao.hashCode());
-		result = prime * result + ((itens == null) ? 0 : itens.hashCode());
-		result = prime * result
-				+ ((modeloCarro == null) ? 0 : modeloCarro.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(valor);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		VersaoCarro other = (VersaoCarro) obj;
-		if (acessorios == null) {
-			if (other.acessorios != null)
-				return false;
-		} else if (!acessorios.equals(other.acessorios))
-			return false;
-		if (descricao == null) {
-			if (other.descricao != null)
-				return false;
-		} else if (!descricao.equals(other.descricao))
-			return false;
-		if (itens == null) {
-			if (other.itens != null)
-				return false;
-		} else if (!itens.equals(other.itens))
-			return false;
-		if (modeloCarro == null) {
-			if (other.modeloCarro != null)
-				return false;
-		} else if (!modeloCarro.equals(other.modeloCarro))
-			return false;
-		if (Double.doubleToLongBits(valor) != Double
-				.doubleToLongBits(other.valor))
-			return false;
-		return true;
-	}
+	
 	public VersaoCarro() {
 		super();
 	}
